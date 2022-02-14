9cbaede7c28eacf3e8f4a86831525789ad8648d4
DarcyShads <87458838+DarcyShads@users.noreply.github.com>
2022-02-14 16:41:16 +0530

    Add files via upload

---
+++ request.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+    <h1>Welcome <?php echo $_POST["name"]; ?></h1>
+    Father's Name : <?php echo $_POST["father_name"]; ?>
+    <br>
+    Addrress : <?php echo $_POST["person_address"]; ?>
+    <br>
+    Gender : <?php echo $_POST["sex"]; ?>
+    <br>
+    Pincode : <?php echo $_POST["pincode"]; ?>
+    <br>
+    Phone No : <?php echo $_POST["pno"]; ?>
+    <br>
+    Email ID : <?php echo $_POST["email"]; ?>
+</body>
+</html>
